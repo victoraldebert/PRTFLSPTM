61ca693714cbd4a6c647a1a8b6b5dc48e7b450b4
Victor <91267367+victoraldebert@users.noreply.github.com>
2022-08-29 15:46:42 +0200

    first commit

---
+++ public/js/scroll.js
@@ -0,0 +1,73 @@
+const sr = ScrollReveal();                  //Méthode de l'effet
+
+
+sr.reveal('.col-md-5', {
+    origin: 'top',
+    distance: '50px',
+    scale: 0.87,
+    reset: true,
+    duration: 2250  
+});    
+
+sr.reveal('.featurette-heading-1', {
+    origin: 'left',
+    distance: '50px',
+    scale: 0.87,
+    reset: true,
+    duration: 2250  
+});    
+
+sr.reveal('.featurette-heading-2', {
+    origin: 'left',
+    distance: '50px',
+    scale: 0.87,
+    reset: true,
+    duration: 2700  
+});  
+
+
+sr.reveal('.col-lg-4', {
+    origin: 'top',
+    distance: '50px',
+    reset: true,
+
+    duration: 2500
+});    
+
+
+
+sr.reveal('.featurette-heading', {
+    origin: 'bottom',
+    distance: '50px',
+    reset: true,
+
+    duration: 2000
+});
+
+
+
+
+sr.reveal('.lead', {
+    origin: 'bottom',
+    distance: '40px',
+    reset: true,
+
+    duration: 2250
+});
+
+
+sr.reveal('.btn-secondary', {
+    origin: 'bottom',
+    distance: '30px',
+    reset: true,
+
+    duration: 2750
+});
+
+sr.reveal('.btn-secondary-two', {
+    origin: 'bottom',
+    distance: '30px',
+    reset: true,
+
+    duration: 3000
+});
